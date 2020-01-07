b543dc3e61b5a92d498aed6694e4d71cae57d012
Reza Rastak <rastak@stanford.edu>
2020-01-06 22:59:32 -0800

    some compile errors fixed

--- test/linear_elastic.cc
+++ test/linear_elastic.cc
@@ -7,13 +7,13 @@ using namespace constitutive;
 
 TEST_CASE("linear elastic", "[mech]") {
   using config = configs::DealII;
-  using MatType LinearElastic<config>;
+  using MatType = LinearElastic<config>;
   LambdaMu<typename config::ParamScalar> elastic_param{100., 60.};
   MatType material{elastic_param};
-  SCENARIO("dim  = 1") {
+  SECTION("dim  = 1") {
     MatType::SmallStrainState<1> state{};
     state.strain = decltype(state.strain){{{0.1}}};
     material.small_strain_update({}, state);
-    ASSERT(state.stress[0][0] = 15.);
+    REQUIRE(state.stress[0][0] = 15.);
   }
 }
